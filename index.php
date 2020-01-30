2ad475b7ef7b094b56afc4b1ac8de513b7d68e5a
aeminkyr <aeminkyr@gmail.com>
2020-01-30 12:37:13 +0300

    index.php minified

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
-
-
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 </head>
 <body>
 	<nav class="navbar sticky-top navbar-light border-bottom">
@@ -18,10 +15,8 @@
 			<input type="text" placeholder="Search..">
 			<button type="submit"><i class="fa fa-search"></i></button>
 		</form>
-
 		<a style="float: right;" href="#login">login</a>
 	</nav>
-	
 	<ul class="nav" id="nav-items" style="">
 		<li class="nav-item">
 			<a class="nav-link active" href="#">#objectoriented</a>
@@ -36,8 +31,6 @@
 			<a class="nav-link" href="#" tabindex="-1" >#something</a>
 		</li>
 	</ul>
-
-
 	<div class="jumbotron" style="background-color: white;">
 		<h1 class="display-4">Hello, world!</h1>
 		<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
@@ -45,9 +38,6 @@
 		<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 		<a class="btn btn-primary btn-lg" style="background-color: black; color: white;" href="#" role="button">Learn more</a>
 	</div>
-
-
-
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
